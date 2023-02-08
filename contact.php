462ed5c8b5413f36ebbbe24499c1788203463121
Ritik sharma <98308171+Emritik@users.noreply.github.com>
2023-02-08 18:56:54 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,6 +3,7 @@
 
 if(isset($_POST['txtName']))
 {
+	
 	// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 	
 $con = mysqli_connect('localhost', 'root', '','db_contact');
